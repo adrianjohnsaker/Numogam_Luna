2df7eab441b47e17f16403001f32d526dbc7f2d2
adrianjohnsaker <adriansaker@outlook.com>
2025-03-09 14:46:10 +0000

    Update enhanced_module_downloader.py

--- numogram_code/enhanced_module_downloader.py
+++ numogram_code/enhanced_module_downloader.py
@@ -12,8 +12,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ModuleDownloader extends AppCompatActivity {
     
