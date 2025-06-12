1cb146802c1840d86fbe265cf757f7e779603434
adrianjohnsaker <adriansaker@outlook.com>
2025-06-12 17:51:44 +0100

    Create SettingsGradle.kt

---
+++ SettingsGradle.kt
@@ -0,0 +1,25 @@
+/ settings.gradle
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AmeliaConsciousness"
+include ':app'
+
+// gradle.properties
+org.gradle.jvmargs=-Xmx2048m -Dfile.encoding=UTF-8
+android.useAndroidX=true
+kotlin.code.style=official
+android.nonTransitiveRClass=true
+android.enableJetifier=true
+python.disableLifecycleHook=false
